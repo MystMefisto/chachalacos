c82f2a94a187bae7e1c26bbd77de9e4440eb0013
--global <carlosteiker@gmail.com>
2021-04-16 21:01:19 -0500

    Embedded yt added

--- public/js/app.js
+++ public/js/app.js
@@ -37421,9 +37421,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /mnt/c/Users/luisito/desktop/web development/blogPodcast/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /mnt/c/Users/luisito/desktop/web development/blogPodcast/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /mnt/c/Users/luisito/desktop/web development/blogPodcast/resources/sass/styles.sass */"./resources/sass/styles.sass");
+__webpack_require__(/*! /mnt/c/users/luisito/desktop/web development/blogPodcast/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /mnt/c/users/luisito/desktop/web development/blogPodcast/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /mnt/c/users/luisito/desktop/web development/blogPodcast/resources/sass/styles.sass */"./resources/sass/styles.sass");
 
 
 /***/ })
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,18 +7,30 @@
     <title>Chachalacos podcast</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <link rel="icon" href="{{asset('images/logo.svg')}}">
 </head>
 <body>
     <!-- Logo -->
-    <nav class="navbar navbar-light bg-main">
-        <div class="container p-1">
-            <a class="navbar-brand m-auto" href="#">
-                <img src="{{asset('images/logo.svg')}}" width="100" alt="" loading="lazy">
-            </a>
-        </div>
-    </nav>
 
+        <nav class="navbar navbar-default w-100 position-fixed navbar-light bg-main">
+            <div class="container p-1">
+                <a class="navbar-brand " href="#">
+                    <img src="{{asset('images/logo.svg')}}" width="100" alt="" loading="lazy">
+                </a>
+            </div>
+        </nav>
+
+
+    <div class="pt-5" {{ isset($categoryIdSelected)? 'hidden': '' }}>
+        <h1 class="pt-5 text-center">Chachalacos podcast</h1>
+        <div class="container-fluid content">
+             <img class="d-block mx-auto" src="{{asset('images/logo.svg')}}" width="500" alt="" loading="lazy">
+        </div>
+    </div>
        <!-- Contenido -->
+       <div class="container-fluid content" {{ isset($categoryIdSelected)? 'hidden': '' }}>
+            <iframe class="latestVideoEmbed d-block mx-auto" vnum='0' cid="UC8rFGNXZeskEgJ25_xa3VKQ" width="600" height="340" frameborder="0" allowfullscreen></iframe>
+       </div>
        <section class="container-fluid content">
         <!-- Categorías -->
         <div class="row justify-content-center">
@@ -142,5 +154,35 @@
         </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script>
+        var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");
+        function loadVideo(iframe) {
+          $.getJSON(reqURL + iframe.getAttribute('cid'),
+            function(data) {
+              var videoNumber = (iframe.getAttribute('vnum') ? Number(iframe.getAttribute('vnum')) : 0);
+              console.log(videoNumber);
+              var link = data.items[videoNumber].link;
+              id = link.substr(link.indexOf("=") + 1);
+              iframe.setAttribute("src", "https://youtube.com/embed/" + id + "?controls=1");
+            }
+          );
+        }
+        var iframes = document.getElementsByClassName('latestVideoEmbed');
+        for (var i = 0, len = iframes.length; i < len; i++) {
+          loadVideo(iframes[i]);
+        }
+
+        var prevScrollpos = window.pageYOffset;
+        window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+        } else {
+        document.getElementById("navbar").style.top = "-50px";
+        }
+        prevScrollpos = currentScrollPos;
+        }
+        </script>
 </body>
 </html>
